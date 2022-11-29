9bfa81b0e4b3935708b7a48af0909a48375eb08f
Victor Paranhos <victor_paranhos@hotmail.com>
2022-11-29 16:33:38 -0300

    Fim2

--- js.js
+++ js.js
@@ -4,19 +4,16 @@ let inputNumber = document.querySelector('#btn-numero')
 let botaoTentar = document.querySelector('#btn-tentar')
 let botaJogarNovamente = document.querySelector('#btn-jogar-novamente')
 let botaTentarNovamente = document.querySelector('#btn-tentar-novamente')
-
+inputNumber.focus()
 botaJogarNovamente.addEventListener("click", jogarnovamente)
 botaoTentar.addEventListener("click", rodarjogo)
 botaTentarNovamente.addEventListener("click", function tentarnovamente(){
     
     document.querySelector('.caixa-1').classList.remove("hide")
     document.querySelector('.caixa-3').classList.add("hide")
+    inputNumber.focus()
     
 })
-
-function tentarnovamente() {
-   
-}
 
 
 let contagem = 1
@@ -33,6 +30,7 @@ function rodarjogo(){
     if(inputNumber.value > 10 || inputNumber.value < 0) {
         alert("Escolha Números entre 0 e 10")
         inputNumber.value = ''
+        inputNumber.focus()
 
     } else if(Number(inputNumber.value) == numerRandomico) {
         document.querySelector('.texto-tentativa').innerText = `Acertou em ${contagem} ${plural(contagem)}`
@@ -41,12 +39,14 @@ function rodarjogo(){
         numerRandomico = Math.round(Math.random() * 10)
         inputNumber.value = ''
         contagem = 1
+        inputNumber.focus()
 
     }else {
         document.querySelector('.caixa-1').classList.add("hide")
         document.querySelector('.caixa-3').classList.remove("hide")
         inputNumber.value = ''
         contagem++
+        inputNumber.focus()
 
         
     }
